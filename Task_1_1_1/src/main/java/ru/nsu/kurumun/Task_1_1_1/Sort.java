0d13290144f8f1fc19e513ac4b18d66f6b4ebc79
mibombo <mihailkalantaev@gmail.com>
2026-09-09 16:29:51 +0700

    hippo ready

--- Task_1_1_1/src/main/java/ru/nsu/kurumun/Task_1_1_1/Sort.java
+++ Task_1_1_1/src/main/java/ru/nsu/kurumun/Task_1_1_1/Sort.java
@@ -1,7 +1,31 @@
 package ru.nsu.kurumun.Task_1_1_1;
 
 public class Sort {
-    public static int[] sort(int[] array){
-        return array;
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
+    static void hippo(int[] arr, int n, int i) {
+        int larg = i;
+        int lt = 2*i + 1;
+        int rt = 2*i + 2;
+
+        if (lt<n && arr[lt]>arr[larg]) larg = lt;
+        if (rt<n && arr[rt]>arr[larg]) larg = rt;
+
+        if (larg != i) {
+            swap(arr, i, larg);
+            hippo(arr, n, larg);
+        }
+    }
+    public static void sort(int[] arr) {
+        int n = arr.length;
+        for (int i = n / 2 - 1; i >= 0; i--) hippo(arr, n, i);
+        for (int i = n - 1; i > 0; i--) {
+            swap(arr, 0, i);
+            hippo(arr, i, 0);
+        }
+    }
+
 }
--- Task_1_1_1/src/test/java/ru/nsu/kurumun/Task_1_1_1/SortTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/kurumun/Task_1_1_1/SortTest.java
@@ -7,9 +7,70 @@ import static org.junit.jupiter.api.Assertions.*;
 class SortTest {
 
     @Test
-    void sort() {
-        int[] array = new int[]{1, 2, 3};
-        var result = Sort.sort(array);
-        assertArrayEquals(new int[]{1, 2, 3}, result);
+    void sortA() {
+        int[] array = {1, 2, 3, 4, 5};
+        Sort.sort(array);
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, array);
+    }
+
+    @Test
+    void sortB() {
+        int[] array = {5, 4, 3, 2, 1};
+        Sort.sort(array);
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, array);
+    }
+
+    @Test
+    void sortC() {
+        int[] array = {3, 1, 4, 1, 5, 9, 2, 6, 5};
+        Sort.sort(array);
+        assertArrayEquals(new int[]{1, 1, 2, 3, 4, 5, 5, 6, 9}, array);
+    }
+
+    @Test
+    void sortD() {
+        int[] array = {};
+        Sort.sort(array);
+        assertArrayEquals(new int[]{}, array);
+    }
+
+    @Test
+    void sortE() {
+        int[] array = {42};
+        Sort.sort(array);
+        assertArrayEquals(new int[]{42}, array);
+    }
+
+    @Test
+    void sortF() {
+        int[] array = {-5, 3, -1, 0, 8, -8};
+        Sort.sort(array);
+        assertArrayEquals(new int[]{-8, -5, -1, 0, 3, 8}, array);
+    }
+
+    @Test
+    void sortG() {
+        int[] array = {7, 7, 7, 7, 7};
+        Sort.sort(array);
+        assertArrayEquals(new int[]{7, 7, 7, 7, 7}, array);
+    }
+
+    @Test
+    void sortH() {
+        int[] array = {2, 1};
+        Sort.sort(array);
+        assertArrayEquals(new int[]{1, 2}, array);
+    }
+
+    @Test
+    void sortI() {
+        int[] array = new int[1000];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = array.length - i;
+        }
+        Sort.sort(array);
+        for (int i = 0; i < array.length; i++) {
+            assertEquals(i + 1, array[i]);
+        }
     }
 }
